51e7a5ef03eec4ba9a921efb3ac01fe11df0c25c
vlebourl <vlebourl@gmail.com>
2022-04-26 09:54:04 +0200

    add diagnostics

---
+++ custom_components/vesync/diagnostics.py
@@ -0,0 +1,23 @@
+"""Provides diagnostics for VeSync."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    data = hass.data[DOMAIN][entry.entry_id]
+    devices = {"fans": [], "outlets": [], "switches": [], "bulbs": []}
+    for type in ["fans", "outlets", "switches", "bulbs"]:
+        for d in data["manager"]._dev_list[type]:
+            devices[type].append(
+                {"device": d.config_dict, "config": d.config, "details": d.details}
+            )
+    return devices

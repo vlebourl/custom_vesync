3c537e26407550081583d691de6b406acb3783aa
vlebourl <vlebourl@gmail.com>
2022-04-27 20:49:23 +0200

    rename a key in diagnostics.

--- custom_components/vesync/diagnostics.py
+++ custom_components/vesync/diagnostics.py
@@ -23,7 +23,7 @@ async def async_get_config_entry_diagnostics(
         for d in data["manager"]._dev_list[type]:
             devices[type].append(
                 {
-                    "device": _if_has_attr_else_none(d, "config_dict") or {},
+                    "config_dict": _if_has_attr_else_none(d, "config_dict") or {},
                     "config": _if_has_attr_else_none(d, "config") or {},
                     "details": _if_has_attr_else_none(d, "details") or {},
                 }

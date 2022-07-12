5c15cd671d2937b8482ddaad5227720fa46c8b5a
Vincent Le Bourlot <vlebourl@gmail.com>
2022-07-12 09:01:59 +0200

    fix regression from 0.1.10

--- custom_components/vesync/common.py
+++ custom_components/vesync/common.py
@@ -59,8 +59,8 @@ async def async_process_devices(hass, manager):
             else:
                 if hasattr(fan, "config_dict"):
                     devices[VS_NUMBERS].append(fan)
-                if "air_quality" in fan.config_dict["features"]:
-                    devices[VS_SENSORS].append(fan)
+                    if "air_quality" in fan.config_dict["features"]:
+                        devices[VS_SENSORS].append(fan)
                 devices[VS_SWITCHES].append(fan)  # for automatic stop and display
                 devices[VS_FANS].append(fan)
         _LOGGER.info("%d VeSync fans found", len(manager.fans))
